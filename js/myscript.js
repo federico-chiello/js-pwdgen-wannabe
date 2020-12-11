1095f58e98c788babd08320a0513b38d684379a5
Fede <chiellofederico@gmail.com>
2020-12-11 15:30:10 +0100

    inserimento var

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,4 @@
 // Chiedi all’utente il suo nome, poi il suo cognome, poi il suo colore preferito.
-// Infine scrivi sulla pagina nomecognomecolorepreferito19.
+// Infine scrivi sulla pagina nomecognomecolorepreferito20.
+
+var nome, cognome, colore;
