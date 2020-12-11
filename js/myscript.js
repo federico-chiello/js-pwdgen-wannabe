0e23f99fd0eae749d50c09a0f568e702d3fe417b
Fede <chiellofederico@gmail.com>
2020-12-11 15:36:52 +0100

    inserimento prompt

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,10 @@
 // Chiedi all’utente il suo nome, poi il suo cognome, poi il suo colore preferito.
 // Infine scrivi sulla pagina nomecognomecolorepreferito20.
 
-var nome, cognome, colore;
+var nome, cognome, colorePreferito;
+
+nome = prompt('Inserisci il tuo nome:');
+
+cognome = prompt('Inserisci il tuo cognome:');
+
+colorePreferito = prompt('Inserisci il tuo colore preferito:');
